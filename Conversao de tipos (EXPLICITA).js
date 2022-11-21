a8f5b8745ee84171fb5538bda8590f18a7e07578
Gustavo Villeth <116782583+Disrrptt@users.noreply.github.com>
2022-11-21 16:45:27 -0300

    Add files via upload

---
+++ Conversao de tipos (EXPLICITA).js
@@ -0,0 +1,16 @@
+// conversao de tipos (Conversoes)
+
+// tipos de dados
+//Booleanos 
+
+// conversao explicita 
+
+const numero = 469;
+const numeroString = Number ("469a"); // quando ocorre caracter solto em um numero ocorre NaN
+
+//Number() 
+//String()
+
+
+
+console.log(numero + Number (numeroString))// ocorre uma concatenacao na string se nao usar a conversao da string 
---
+++ Conversao de tipos.js
@@ -0,0 +1,17 @@
+// conversao de tipos (Conversoes)
+
+// tipos de dados
+//Booleanos 
+
+// conversao implicita 
+
+const numero = 469;
+const numeroString = Number ("469a");
+
+//Number() 
+//String()
+
+
+
+console.log(numero + Number (numeroString))// ocorre uma concatenacao na string se nao usar a conversao da string 
+
