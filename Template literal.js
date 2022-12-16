f94033d9ab56c0cc5de29102f50b44d5833513d3
Gustavo Villeth <116782583+Disrrptt@users.noreply.github.com>
2022-12-16 15:40:26 -0300

    Add files via upload

---
+++ Template literal.js
@@ -0,0 +1,14 @@
+const nome = "Gustavinho";
+const idade =  2022-2004;
+const cidadeNascimento = "Rio de janeiro";
+
+const apresentacao = "meu nome e" + nome + ", minha idade e " + idade + "e nasci na cidade de " + cidadeNascimento ;
+console.log (apresentacao)
+// COMO FICA EM TEMPLATE LITERAL
+
+
+const apresetacao ='meu nome e ${nome}, minha idade e ${idade} e nasci na cidade de ${cidadeNascimento}';  
+
+console.log(apresetacao)
+
+//fica bem mais facil e rapido de se escrever
